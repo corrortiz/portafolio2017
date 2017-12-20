aa817cffd4259510fbb424d5ff3afd22555e1f4e
corrortiz <a.ortizcrr@gmail.com>
2017-12-20 14:54:00 -0600

    first app bar implementation

--- client/portfolio2017/src/Components/HeaderAppBar/HeaderAppBar.js
+++ client/portfolio2017/src/Components/HeaderAppBar/HeaderAppBar.js
@@ -3,14 +3,16 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import SimpleBottomNavigation from '../BottonNavigation/SimpleBottomNavigation';
 
 const HeaderAppBar = () => {
   return (
-    <AppBar position="static" className="header">
-      <Toolbar>
+    <AppBar position="fixed" className="header">
+      <Toolbar className="header__toolbar">
         <Typography type="title" color="inherit" className="header__title">
           AO HyS
         </Typography>
+        <SimpleBottomNavigation />
       </Toolbar>
     </AppBar>
   );
