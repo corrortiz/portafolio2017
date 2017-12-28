4081f5cd9ad5f284d5abe9e7144d6b49bccdf81a
corrortiz <a.ortizcrr@gmail.com>
2017-12-28 12:52:47 -0600

    HOC for redux globals

--- client/portfolio2017/src/HOC/GlobalsConnect/GlobalsConnect.js
+++ client/portfolio2017/src/HOC/GlobalsConnect/GlobalsConnect.js
@@ -1,20 +1,37 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const GlobalsConnect = WrappedComponent =>
-  class NameWrappedComponent extends WrappedComponent {
+/**
+ * This is a HOC for the globals redux state
+ * @param {*Componetn to recibe globals state} WrappedComponent
+ * First a function is created that receives the component that we are going to wrap as a
+ * parameter, this function returns the wrapped component to which the new properties are added
+ * through Redux Connect, which are chosen by the functions mapStateToProps and mapDispachToProps
+ */
+const GlobalsConnect = WrappedComponent => {
+  /**
+   * New class that is necessary to connect to redux
+   */
+  class NameWrappedComponent extends Component {
     render() {
-      const newProps = this.props.globals;
-
-      return <WrappedComponent {...this.props} {...newProps} />;
+      return <WrappedComponent {...this.props} />;
     }
-  };
+  }
 
-function mapStateToProps(state) {
-  return {
-    globals: state.globals
+  /**
+   * Here we choose the redux states to pass
+   * @param {*} state
+   */
+  const mapStateToProps = state => {
+    return {
+      globals: state.globals
+    };
   };
-}
+  /**
+   * Here we connect the react-redux HOC and the new class
+   */
+  return connect(mapStateToProps, undefined)(NameWrappedComponent);
+};
 
-export default connect(mapStateToProps)(GlobalsConnect);
+export default GlobalsConnect;
