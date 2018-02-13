6bde50d0a78dc765938a9edb558091ac6c1ef8c4
corrortiz <a.ortizcrr@gmail.com>
2018-02-12 19:43:27 -0600

    adding service cards

--- client/portfolio2017/src/Components/Routs/ServicesRoute/ServicesRoute.js
+++ client/portfolio2017/src/Components/Routs/ServicesRoute/ServicesRoute.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import ImageContainer from '../../ImageContainer/ImageContainer';
+import Grid from 'material-ui/Grid';
 //Internal Components
-
+import ImageCard from '../../ImageCard/ImageCard';
 /**
  * layout of the service roout
  */
 const ServicesRoute = () => {
   return (
     <div>
-      <ImageContainer />
+      <Grid
+        container
+        spacing={16}
+        alignItems={'center'}
+        justify={'center'}
+        alignContent={'center'}
+      >
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        spacing={8}
+        alignItems={'center'}
+        justify={'center'}
+        alignContent={'center'}
+      >
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={3} lg={3}>
+          <ImageCard />
+        </Grid>
+      </Grid>
     </div>
   );
 };
